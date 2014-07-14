302b504828f3c23875ce9f4404a67d72cdbe6640
yashap <ypodeswa@gmail.com>
2014-07-13 23:06:18 -0700

    Finished both tweet sentiment and term sentiment assignments

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -1,17 +1,62 @@
 import sys
+import csv
+import json
 
-def hw():
-    print 'Hello, world!'
+# Designed to be run from terminal, with this command
+# python term_sentiment.py AFINN-111.txt output.txt
 
-def lines(fp):
-    print str(len(fp.readlines()))
+# Load AFINN file
+def loadSentiment(fname):
+	scores = {}
+	with open(fname, 'rb') as f:
+		content = csv.reader(f, delimiter="\t")
+		for line in content:
+			scores[line[0]] = int(line[1])
+	return scores
+
+# Load Tweets
+def loadTweets(fname):
+	tweets = []
+	with open(fname, 'rb') as f:
+		for line in f:
+			tweets.append(json.loads(line))
+	return tweets
+
+# Print tweet scores
+def newTermScore(tweets, scores):
+	new_terms = {}
+
+	for tweet in tweets:
+		tweet_score = 0
+		text = tweet.get("text")
+		if not tweet.get("text"):
+			text = ""
+		words = text.split()
+
+		for word in words:
+			word_score = scores.get(word)
+			if not word_score:
+				word_score = 0
+				if word not in new_terms:
+					new_terms[word] = 0
+			tweet_score += word_score
+
+		for word in words:
+			if word not in scores:
+				new_terms[word] += tweet_score
+
+	return new_terms
+
+# Print dictionary in "key value" format
+def printDict(my_dict):
+	for item in my_dict:
+		print("%s %s" % (item, my_dict[item]))
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+	scores = loadSentiment(sys.argv[1])
+	tweets = loadTweets(sys.argv[2])
+	new_terms = newTermScore(tweets, scores)
+	printDict(new_terms)
 
 if __name__ == '__main__':
-    main()
+	main()
